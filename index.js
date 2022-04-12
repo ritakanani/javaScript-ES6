cef070eb5a8006396f9d1f04499ab5706b2ffa96
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-04-12 15:06:28 -0400

    toUpperCase() & toLowerCase()

--- index.js
+++ index.js
@@ -15,3 +15,32 @@ console.log(animals.slice(-2));
 // expected output: Array ["duck", "elephant"]
 console.log(animals.slice(2, -1));
 // expected output: Array ["camel", "duck"]
+
+
+/*********************************************/
+
+var caseLetter = "rITa";  
+caseLetter = caseLetter.toUpperCase();
+console.log(caseLetter);   // RITA
+
+caseLetter = caseLetter.toLowerCase();
+console.log(caseLetter);   // rita
+
+// Capitalize the first letter
+// a isolate the first char with slice()
+var firstChar = caseLetter.slice(0, 1);
+
+// b Turn the first char to upper case
+var upperCaseFirstChar = firstChar.toUpperCase();
+
+// c Isolate the rest of the name
+var restOfChar = caseLetter.slice(1, caseLetter.length);
+
+// d Change the rest of the name with lower case
+restOfChar = restOfChar.toLowerCase();
+
+// e concatenate the first char with the rest of the char
+var capitalizeCaseLetter = upperCaseFirstChar + restOfChar;
+console.log(capitalizeCaseLetter);  // Rita
+
+
